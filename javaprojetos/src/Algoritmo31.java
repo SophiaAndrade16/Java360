7d1cbc2efce19edb5d74c0b76fa1f6e335239680
andradesophia271-ops <andradesophia271@gmail.com>
2026-09-15 11:43:48 -0300

    Corrige sintaxe e implementa Algoritmo31

--- javaprojetos/src/Algoritmo31.java
+++ javaprojetos/src/Algoritmo31.java
@@ -1,3 +1,9 @@
 public class Algoritmo31 {
-    
+   
+   public String getAloMundo(){
+        return "Hello World GET!";
+   }
+   public void printarNaTela(){
+     IO.print( "Alo Mundo! Esse Printa!");
+   }
 }
